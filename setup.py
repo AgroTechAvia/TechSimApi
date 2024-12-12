3f1902429c88a0ea5b82be2af4372e81ac64de71
User <ash.senya@gmail.com>
2024-12-12 16:07:31 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "matplotlib==3.9.3",
         "msgpack-python==0.5.6",
         "msgpack-rpc-python==0.4.1",
-        "numpy==2.2.0",
+        "numpy==1.26.4",
         "opencv-python==4.10.0.84",
         "packaging==24.2",
         "pillow==11.0.0",
