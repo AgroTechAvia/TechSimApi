8025a7ae0ab6d8c419787c5f991b6469f59139ee
senya ashlapov <ash.senya@gmail.com>
2025-12-18 15:48:08 +0300

    add high level class

--- agrotechsimapi/__init__.py
+++ agrotechsimapi/__init__.py
@@ -2,3 +2,6 @@ from agrotechsimapi.client import SimClient
 from agrotechsimapi.client import CaptureType
 from agrotechsimapi.video_pb2 import *
 from agrotechsimapi.video_pb2_grpc import *
+
+from agrotechsimapi.pid import PID
+from agrotechsimapi.high_level_client import HighLevelSimClient
